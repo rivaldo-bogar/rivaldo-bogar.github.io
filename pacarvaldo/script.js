f0d94c51e3968ea8fe9e949ceeae0fac8b1d4064
rivaldo-bogar <androsbgr13@gmail.com>
2024-08-13 01:05:33 +0800

    make new page, me and my girlfriend day reamaning

---
+++ pacarvaldo/script.js
@@ -0,0 +1,22 @@
+const startDate = new Date('2023-03-23T00:00:00');
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
+
+function updateElapsedTime() {
+    const now = new Date();
+    const elapsedTime = now - startDate;
+
+    const days = Math.floor(elapsedTime / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((elapsedTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+
+    daysElement.textContent = days;
+    hoursElement.textContent = hours;
+    minutesElement.textContent = minutes;
+    secondsElement.textContent = seconds;
+}
+
+setInterval(updateElapsedTime, 1000);
